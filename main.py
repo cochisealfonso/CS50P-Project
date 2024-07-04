677a1a6c87b6e575edd78d9014c47c560aae8f76
cochisealfonso <delacruzcochise@gmail.com>
2024-07-04 12:52:15 +0800

    Set window and title icons Change character position to the left

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
+
+# Set window title and icon
+pygame.display.set_caption("Project Game")
+cowboy_icon = pygame.image.load("icons\cowboy_icon.png")
+pygame.display.set_icon(cowboy_icon)
+
 clock = pygame.time.Clock()
 running = True
+dt = 0
+
+player_pos = pygame.Vector2(40, screen.get_height() / 2)
 
 while running:
     for event in pygame.event.get():
@@ -11,8 +20,23 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill("white")
+    screen.fill((192,192,192))
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -=300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y +=300 * dt
+    # if keys[pygame.K_a]:
+        # while player_pos.x != 0:
+    #     player_pos.x -= 300 * dt
+    # if keys[pygame.K_d]:
+        # while player_pos.x != screen.get_width():
+        # player_pos.x += 300 * dt
+        
     pygame.display.flip()
-    clock.tick(60)
+    dt = clock.tick(120) / 1000
 
 pygame.quit()
