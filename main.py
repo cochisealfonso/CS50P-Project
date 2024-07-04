80be3162be995b834c8e81638a7d0a5113afaba2
cochisealfonso <delacruzcochise@gmail.com>
2024-07-04 15:24:54 +0800

    Add player image and directional controls

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 
+# Initialize Pygame and display
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
@@ -8,34 +9,42 @@ pygame.display.set_caption("Project Game")
 cowboy_icon = pygame.image.load("icons\cowboy_icon.png")
 pygame.display.set_icon(cowboy_icon)
 
+# Variables
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(40, screen.get_height() / 2)
+# Define player position
+cowboyImage = pygame.image.load("icons\cowboy_player.png")
+playerXPos = 30
+playerYPos = screen.get_height() / 2
+
+# Display player function
+def player():
+    screen.blit(cowboyImage, (playerXPos, playerYPos))
 
 while running:
+    screen.fill((255,204,153))
     for event in pygame.event.get():
         # pygame.QUIT means if the user clicks X, window closes
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((192,192,192))
+    # Display player on screen
+    player()
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
-
+    # Directional controls for the player
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -=300 * dt
+        playerYPos -=300 * dt
     if keys[pygame.K_s]:
-        player_pos.y +=300 * dt
-    # if keys[pygame.K_a]:
-        # while player_pos.x != 0:
-    #     player_pos.x -= 300 * dt
-    # if keys[pygame.K_d]:
-        # while player_pos.x != screen.get_width():
-        # player_pos.x += 300 * dt
-        
+        playerYPos +=300 * dt
+    if keys[pygame.K_a]:
+        playerXPos -= 300 * dt
+    if keys[pygame.K_d]:
+        playerXPos += 300 * dt
+    
+
     pygame.display.flip()
     dt = clock.tick(120) / 1000
 
