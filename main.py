9a8420b316fec7ba28ab93fd8f708b066c80ace4
cochisealfonso <delacruzcochise@gmail.com>
2024-07-05 14:13:31 +0800

    Setup player movement mechanics and limits

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
+from player import Player
 
 # Initialize Pygame and display
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
+player = Player(playerType=1)
+
 
 # Set window title and icon
 pygame.display.set_caption("Project Game")
@@ -15,7 +18,7 @@ running = True
 dt = 0
 
 # Define player position
-cowboyImage = pygame.image.load("icons\cowboy_player.png")
+cowboyImage = pygame.image.load("icons\cowboy_player1.png")
 playerXPos = 30
 playerYPos = screen.get_height() / 2
 
@@ -24,27 +27,38 @@ def player():
     screen.blit(cowboyImage, (playerXPos, playerYPos))
 
 while running:
-    screen.fill((255,204,153))
     for event in pygame.event.get():
         # pygame.QUIT means if the user clicks X, window closes
         if event.type == pygame.QUIT:
             running = False
-
+    
     # Display player on screen
+    screen.fill((255,204,153))
     player()
 
     # Directional controls for the player
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        playerYPos -=300 * dt
+        if playerYPos > (0): 
+            playerYPos -= 300 * dt
+        else:
+            playerYPos += 350 * dt
     if keys[pygame.K_s]:
-        playerYPos +=300 * dt
+        if playerYPos < (screen.get_height() - 60):
+            playerYPos += 300 * dt
+        else:
+            playerYPos -= 350 * dt
     if keys[pygame.K_a]:
-        playerXPos -= 300 * dt
+        if playerXPos > (0): 
+            playerXPos -= 300 * dt
+        else:
+            playerXPos += 350 * dt
     if keys[pygame.K_d]:
-        playerXPos += 300 * dt
+        if playerXPos < (screen.get_width() / 8):
+            playerXPos += 300 * dt
+        else:
+            playerXPos -= 350 * dt
     
-
     pygame.display.flip()
     dt = clock.tick(120) / 1000
 
