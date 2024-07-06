b7327ded732e2c865219d80d93b1f5b2711afd88
cochisealfonso <delacruzcochise@gmail.com>
2024-07-06 09:28:27 +0800

    Add move() function

--- player.py
+++ player.py
@@ -1,36 +1,28 @@
 import pygame
 
 class Player:
-    def __init__(self, playerType):
-        self.type = playerType
-
-    # def load(self, screen):
-        # clock = pygame.time.Clock()
-        # dt = 0
-
-        # cowboyImage = pygame.image.load(f"icons\cowboy_player{self.type}.png")
-        # playerXPos = 30
-        # playerYPos = 300
-
-        # screen.blit(cowboyImage, (playerXPos, playerYPos))
-        # return playerXPos, playerYPos
+    def __init__(self, playerType, XPos=30, YPos=300):
+        self.playertype = pygame.image.load(f"icons\cowboy_player{playerType}.png")
+        self.XPos = XPos
+        self.YPos = YPos
 
     # Directional controls for the player
-        # keys = pygame.key.get_pressed()
-        # if keys[pygame.K_w]:
-        #     playerYPos -=300 * dt
-        # if keys[pygame.K_s]:
-        #     playerYPos +=300 * dt
-        # if keys[pygame.K_a]:
-        #     if playerXPos > (0): 
-        #         playerXPos -= 300 * dt
-        #     else:
-        #         playerXPos += 350 * dt
-        # if keys[pygame.K_d]:
-        #     if playerXPos < (screen.get_width() / 8):
-        #         playerXPos += 300 * dt
-        #     else:
-        #         playerXPos -= 350 * dt
+    def move(self):
+        screen = pygame.display.set_mode((800, 600))
+        screen.blit(self.playertype, (self.XPos, self.YPos))
 
-        # pygame.display.flip()
-        # dt = clock.tick(120) / 1000
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            playerYPos -=300 * dt
+        if keys[pygame.K_s]:
+            playerYPos +=300 * dt
+        if keys[pygame.K_a]:
+            if playerXPos > (0): 
+                playerXPos -= 300 * dt
+            else:
+                playerXPos += 350 * dt
+        if keys[pygame.K_d]:
+            if playerXPos < (screen.get_width() / 8):
+                playerXPos += 300 * dt
+            else:
+                playerXPos -= 350 * dt
