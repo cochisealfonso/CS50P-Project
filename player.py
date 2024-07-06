5ed4ad6953ebb8c151ce15c1f6dc8e2c68d77ca7
cochisealfonso <delacruzcochise@gmail.com>
2024-07-06 10:00:33 +0800

    Add movement mechanics to player.py from main.py

--- player.py
+++ player.py
@@ -1,28 +1,33 @@
 import pygame
 
 class Player:
-    def __init__(self, playerType, XPos=30, YPos=300):
+    def __init__(self, playerType, screen, XPos=30, YPos=300):
         self.playertype = pygame.image.load(f"icons\cowboy_player{playerType}.png")
+        self.screen = screen
         self.XPos = XPos
         self.YPos = YPos
 
     # Directional controls for the player
-    def move(self):
-        screen = pygame.display.set_mode((800, 600))
-        screen.blit(self.playertype, (self.XPos, self.YPos))
-
+    def move(self, dt):
+        self.screen.blit(self.playertype, (self.XPos, self.YPos))
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
-            playerYPos -=300 * dt
+            if self.YPos > (0): 
+                self.YPos -= 300 * dt
+            else:
+                self.YPos += 350 * dt
         if keys[pygame.K_s]:
-            playerYPos +=300 * dt
+            if self.YPos < (self.screen.get_height() - 60):
+                self.YPos += 300 * dt
+            else:
+                self.YPos -= 350 * dt
         if keys[pygame.K_a]:
-            if playerXPos > (0): 
-                playerXPos -= 300 * dt
+            if self.XPos > (0): 
+                self.XPos -= 300 * dt
             else:
-                playerXPos += 350 * dt
+                self.XPos += 350 * dt
         if keys[pygame.K_d]:
-            if playerXPos < (screen.get_width() / 8):
-                playerXPos += 300 * dt
+            if self.XPos < (self.screen.get_width() / 8):
+                self.XPos += 300 * dt
             else:
-                playerXPos -= 350 * dt
+                self.XPos -= 350 * dt
